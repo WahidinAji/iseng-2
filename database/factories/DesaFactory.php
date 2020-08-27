15552386ee3b1f1b27f18af307894a7ef8251491
WahidinAji <a17wahidin@gmail.com>
2020-08-27 15:15:07 +0700

    setup awal

---
+++ database/factories/DesaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Desa;
+use Faker\Generator as Faker;
+
+$factory->define(Desa::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
